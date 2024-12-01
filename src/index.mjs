14df62d5a2fe022bfe9ca8414d847b976b7b07de
qingchun <96961592+qingchunnh@users.noreply.github.com>
2024-12-02 07:20:58 +0800

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -23,9 +23,9 @@ const main = (async () => {
         await fetchTarkovDevData(graphQLClient, 'pve');
 
         // Fetch the latest prices.json and handbook.json from SPT-AKI's git repo
-        await downloadFile('https://raw.githubusercontent.com/sp-tarkov/server/refs/heads/3.10.1-dev/project/assets/database/templates/handbook.json', 'akihandbook.json');
-        await downloadFile('https://raw.githubusercontent.com/sp-tarkov/server/refs/heads/3.10.1-dev/project/assets/database/templates/items.json', 'akiitems.json');
-        await downloadFile('https://raw.githubusercontent.com/sp-tarkov/server/refs/heads/3.10.1-dev/project/assets/database/templates/prices.json', 'akiprices.json');
+        await downloadFile('https://raw.githubusercontent.com/sp-tarkov/server/refs/heads/3.10.2-dev/project/assets/database/templates/handbook.json', 'akihandbook.json');
+        await downloadFile('https://raw.githubusercontent.com/sp-tarkov/server/refs/heads/3.10.2-dev/project/assets/database/templates/items.json', 'akiitems.json');
+        await downloadFile('https://raw.githubusercontent.com/sp-tarkov/server/refs/heads/3.10.2-dev/project/assets/database/templates/prices.json', 'akiprices.json');
     }
 
     processData('regular');
